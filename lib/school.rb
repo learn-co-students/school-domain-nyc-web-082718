90f1a6283cdc7bd22ab59035430c5d2e241280d8
github username <github email address>
2018-08-29 20:27:52 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,46 @@
 # code here!
+
+require "pry"
+
+class School
+
+attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+
+    if @roster[grade] == nil
+      @roster[grade] = []
+      @roster[grade] << student
+    else
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    # binding.pry
+    @roster[grade]
+  end
+
+  def sort
+    new_roster = {}
+    roster.each do |grade, name|
+      new_roster[grade] = name.sort
+    end
+    new_roster
+
+    # new_roster[key] = @roster.values.sort
+    @roster[grade] = name.sort
+  end
+
+end
+
+
+fox_lane = School.new("Fox Lane")
+fox_lane.add_student("Jordan", 12)
+fox_lane.add_student("Ollie", 12)
+  # binding.pry
